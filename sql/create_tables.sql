378eff4b8f4d1d188bf59c3209d2a3007e459d71
zhJQ1218 <977274456@qq.com>
2023-12-14 00:14:34 +0800

    修改sql文件

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -52,7 +52,7 @@ CREATE TABLE `score`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `skill`;
 CREATE TABLE `skill`  (
-                          `id` int(10) UNSIGNED ZEROFILL NOT NULL COMMENT '技能展示id',
+                          `id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '技能展示id',
                           `student_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '学生姓名',
                           `student_skill_score` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '学生技能成绩（json）',
                           `is_delete` tinyint(1) UNSIGNED ZEROFILL NULL DEFAULT NULL COMMENT '逻辑删除，0未删除，1已删除',
@@ -75,10 +75,10 @@ CREATE TABLE `skill_index`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `skill_level`;
 CREATE TABLE `skill_level`  (
-                                `id` int(0) NOT NULL COMMENT '技能等级id',
+                                `id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '技能等级id',
                                 `skill_level` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '技能等级',
-                                `end` int(0) NULL DEFAULT NULL COMMENT '结束值',
-                                `start` int(0) NULL DEFAULT NULL COMMENT '开始值',
+                                `end` int NULL DEFAULT NULL COMMENT '结束值',
+                                `start` int NULL DEFAULT NULL COMMENT '开始值',
                                 `is_delete` tinyint(1) UNSIGNED ZEROFILL NULL DEFAULT NULL COMMENT '逻辑删除，0未删除，1已删除',
                                 PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
@@ -93,7 +93,7 @@ CREATE TABLE `user`  (
                          `user_account` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '用户账号',
                          `user_password` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '用户密码',
                          `user_role` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '用户角色',
-                         `student_number` int(0) NULL DEFAULT NULL COMMENT '学生学号',
+                         `student_number` int NULL DEFAULT NULL COMMENT '学生学号',
                          `student_academy_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '学生学院',
                          `student_class` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '学生班级',
                          `student_grade` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '学生年级',
